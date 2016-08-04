df86933ce7338f1f5c8292af9a3496cb5c8735cc
brorbw <brorwinther@gmail.com>
2016-08-04 12:40:59 +0200

    a message

--- Mandatory2/copy.js
+++ Mandatory2/copy.js
@@ -23,40 +23,6 @@ function buildCube(centerPoint, length) {
 }
 
 
-//quad( 1, 0, 3, 2 );
-//quad( 2, 3, 7, 6 );
-//quad( 3, 0, 4, 7 );
-//quad( 6, 5, 1, 2 );
-//quad( 4, 5, 6, 7 );
-//quad( 5, 4, 0, 1 );
-
-var faceColor = [];
-function color() {
-    faceColor.push(vec4(0,0,0,1));
-    y = subtract(vertices[1], vertices[0]); //y vector 01
-    x = subtract(vertices[3], vertices[0]); //x vector 03
-    faceColor.push(vec4(cross(x,y), 1));
-// face b
-    y = subtract(vertices[2], vertices[3]); //y vector 32
-    x = subtract(vertices[7], vertices[3]); //x vector 37
-    faceColor.push(vec4(cross(x, y), 1));
-// face c
-    y = subtract(vertices[0], vertices[4]); //y vector 40
-    x = subtract(vertices[7], vertices[4]); //x vector 47
-    faceColor.push(vec4(cross(x,y), 1));
-// face d
-    y = subtract(vertices[5], vertices[1]); //y vector 15
-    x = subtract(vertices[2], vertices[1]); //x vector 12
-    faceColor.push(vec4(cross(x,y), 1));
-// face e
-    y = subtract(vertices[6], vertices[7]); //y vector 76
-    x = subtract(vertices[4], vertices[7]); //x vector 74
-    faceColor.push(vec4(cross(x,y), 1));
-// face f
-    y = subtract(vertices[5], vertices[4]); //y vector 45
-    x = subtract(vertices[0], vertices[4]); //x vector 40
-    faceColor.push(vec4(cross(x, y), 1));
-}
 
 var near = 1;
 var far = 8;
@@ -75,25 +41,30 @@ const at = vec3(0.0, 0.0, 0.0);
 const up = vec3(0.0, 1.0, 0.0);
 
 function quad(a, b, c, d) {
+
+    var x = subtract(vertices[a],vertices[b]);
+    var y = subtract(vertices[b],vertices[c]);
+    var color = vec4(cross(x,y),1);
+
     pointsArray.push(vertices[a]);
-    colorsArray.push(faceColor[a]);
+    colorsArray.push(color);
     pointsArray.push(vertices[b]);
-    colorsArray.push(faceColor[a]);
+    colorsArray.push(color);
     pointsArray.push(vertices[c]);
-    colorsArray.push(faceColor[a]);
+    colorsArray.push(color);
     pointsArray.push(vertices[a]);
-    colorsArray.push(faceColor[a]);
+    colorsArray.push(color);
     pointsArray.push(vertices[c]);
-    colorsArray.push(faceColor[a]);
+    colorsArray.push(color);
     pointsArray.push(vertices[d]);
-    colorsArray.push(faceColor[a]);
+    colorsArray.push(color);
 }
 
 
 function colorCube(){
     buildCube(vec3(0.0,0.0,0.0),1);
     //buildCube(vec3(0,1,1),1);
-    color();
+
     quad( 1, 0, 3, 2 );
     quad( 2, 3, 7, 6 );
     quad( 3, 0, 4, 7 );
