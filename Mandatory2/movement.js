b2ce1d5575da42c20e733c494bcc0b61b9142ff2
brorbw <brorwinther@gmail.com>
2016-08-05 22:21:29 +0200

    added a camera object

--- Mandatory2/movement.js
+++ Mandatory2/movement.js
@@ -2,5 +2,25 @@ var mvMatrix = lookAt(eye, at , up);
 
 var camera = {
     position: new Position (0,0,0),
-    //do all the other stuff
+    lookLeft: lookLeft,
+    lookRight: lookRight,
+    lookUp: lookUp,
+    lookDown: lookDown,
+}
+
+
+function lookUp(){
+    //do stuff that looks up
+}
+
+function lookDown(){
+    //do stuff that looks down
+}
+
+function lookLeft(){
+    //do stuff that looks left
+}
+
+function lookRight(){
+    //do stuff that looks right
 }
