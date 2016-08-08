51698aeab57689bce40120322d8e25d27de1ab5f
philiprileyitu <panr@itu.dk>
2016-08-08 14:17:53 +0200

    Merged collisions

--- Mandatory2/main.js
+++ Mandatory2/main.js
@@ -106,6 +106,9 @@ var render = function(){
         camera.moveForward();
     }
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    if (!flying) {
+      gravity();
+    }
     gl.uniformMatrix4fv( modelView, false, flatten(mvMatrix) );
     gl.uniformMatrix4fv( projection, false, flatten(pMatrix) );
 
