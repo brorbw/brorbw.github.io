dc3d72e815f6cd02b30286cbb9ecc5a5f7976311
Dagmar <dagmar.bertsche@uni-ulm.de>
2016-08-09 11:28:56 +0200

    fixed add / remove

--- Mandatory2/pickCube.js
+++ Mandatory2/pickCube.js
@@ -1,8 +1,9 @@
 var build = false;
 var framePoints=[];
+
 function allowedToRemove(){
   for (i=1;i<5;i++){
-    var pos = getPosOfBlockInFrontForward(i);
+    var pos = getPosOfBlocksInFront(i);
     var centerFront = posToCenter(pos[0],pos[1],pos[2]);
     var cubeInFront = getCube(centerFront);
     if (cubeInFront !== undefined && cubeInFront !== 0){
@@ -11,31 +12,36 @@ function allowedToRemove(){
        emptyArrays();
        drawWorld();
        resendBuffers();
+       break;
     }
   }
 }
 
 function allowedToBuild(){
   for (i=2;i<5;i++){
-    var pos = getPosOfBlockInFrontForward(i);
+    var pos = getPosOfBlocksInFront(i);
     var centerFront = posToCenter(pos[0],pos[1],pos[2]);
     var cubeInFront = getCube(centerFront);
     // cubeInFront at least at the distance 2
     if (cubeInFront !== undefined && cubeInFront !== 0){
-      var posNew = getPosOfBlockInFrontForward(i-1);
-      var centerNew = posToCenter(posNew[0],posNew[1],posNew[2]);
-      if(build){
-        console.log('build'+centerNew.x+', '+centerNew.y+', '+centerNew.z);
-        var pos = new Position(centerNew.x,centerNew.y,centerNew.z);
-        var box = new Box(pos);
-        addBox(box);
-        drawWorld();
-        resendBuffers();
-        build = false;
-        break;
+      for(j=0;j<11;j++){
+        var posNew = getPosOfBlocksInFront(i-0.1*j);
+        var centerNew = posToCenter(posNew[0],posNew[1],posNew[2]);
+        if(centerNew.x!==centerFront.x || centerNew.y!==centerFront.y ||centerNew.z!==centerFront.z){
+          if(build){
+            console.log('build'+centerNew.x+', '+centerNew.y+', '+centerNew.z);
+            var pos = new Position(centerNew.x,centerNew.y,centerNew.z);
+            var box = new Box(pos);
+            addBox(box);
+            drawWorld();
+            resendBuffers();
+            build = false;
+            break;
+          }
+          drawWireFrame(centerNew.x,centerNew.y,centerNew.z,1);
+          break;
+        }
       }
-      drawWireFrame(centerNew.x,centerNew.y,centerNew.z,1);
-      break;
     } else{
       //console.log('not allowed to build');
     }
@@ -44,6 +50,9 @@ function allowedToBuild(){
 
 
 function drawWireFrame(x,y,z, boxLength){
+  if(init)
+    init= !init;
+    
       framePoints=[];
       colorsDArray=[];
       //second param is suppose to be the size of the cube so we can make
@@ -88,7 +97,7 @@ function drawWireFrame(x,y,z, boxLength){
       for (i=0;i<24;i++){
         colorsDArray.push(vec4(0.0,0.0,0.0,1.0));
       }
- console.log('should dra'+x+', '+y+', '+z);
+// console.log('should dra'+x+', '+y+', '+z);
   gl.bindBuffer(gl.ARRAY_BUFFER, cDBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsDArray), gl.STATIC_DRAW);
 
@@ -97,16 +106,12 @@ function drawWireFrame(x,y,z, boxLength){
       gl.vertexAttribPointer( vDPosition, 2, gl.FLOAT, false, 0, 0 );
       gl.enableVertexAttribArray( vDPosition );
 
-      gl.drawArrays( gl.LINES, 0, 24 );
+  //    gl.drawArrays( gl.LINES, 0, 24 );
 }
 
 
-
-
-function getPosOfBlockInFrontForward(i) {
+function getPosOfBlocksInFront(i) {
   var atVec = subtract(at, eye);
-  var xzAxis = cross(atVec, up);
-  var direction = cross(up, xzAxis);
-  direction = normalize(direction);
+  direction  =normalize(atVec);
   return add(eye, mult(direction, vec3(i+0.3, i+0.3, i+0.3)));
 }
